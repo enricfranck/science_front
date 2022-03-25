6b2a51051535637fa71d72baa725d3cc33c2bf60
franckenric <enricfranck@gmail.com>
2022-03-25 07:02:07 +0100

    delete forlder

--- brouillon/req.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "http://localhost/api/v1/ancien_etudiants/upload_photo/"
-files = {"uploaded_file": open("../image.jpg", 'rb')}
-res = requests.post(url=url, files=files)
-print(res.text)
